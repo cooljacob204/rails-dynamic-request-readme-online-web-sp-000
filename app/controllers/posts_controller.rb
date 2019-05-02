daa2d95fe851fca4b5de035c567cbefbc9376bf4
Jacob Feldman <jacobgfeld@gmail.com>
2019-05-02 22:32:15 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,6 @@ class PostsController < ApplicationController
   def show
     post = Post.find(params[:id])
     
-    render erb content_tag(h1:, post.title), content_tag(p:, post.description)
+    render erb content_tag(:h1, post.title), content_tag(:p, post.description)
   end
 end
